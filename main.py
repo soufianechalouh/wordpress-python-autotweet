16ad189f90a9bb788fa53f63af717bd3bef6c8f1
Soufiane CHALOUH <soufianechalouh@gmail.com>
2021-07-02 23:55:11 +0100

    refactor: Add website URL as env variable initiate code refactoring

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 import html
 import os
 import time
-
 import tweepy
-import json
 import requests
-from config import CONSUMER_KEY, ACCESS_TOKEN, CONSUMER_SECRET, ACCESS_TOKEN_SECRET
+from config import CONSUMER_KEY, ACCESS_TOKEN, CONSUMER_SECRET, ACCESS_TOKEN_SECRET, WEBSITE_URL
 
 
 def create_api():
@@ -19,29 +17,37 @@ def create_api():
     # Create API object
     return tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
 
-#
+
+def main():
+    while True:
+        previous_links = []
+        with open(os.path.join(os.getcwd(), "assets/previous_links.txt"), 'r') as filehandle:
+            for line in filehandle:
+                # remove linebreak which is the last character of the string
+                currentPlace = line[:-1]
+                previous_links.append(currentPlace)
+
+        response = requests.get(f"{WEBSITE_URL}/wp-json/wp/v2/posts/")
+        posts_list = response.json()
+        unpublished_posts = list(filter(lambda x: x["link"] not in previous_links, posts_list))
+        if unpublished_posts:
+            print("posts retrieved")
+
+        with open("assets/previous_links.txt", 'w') as filehandle:
+            for listitem in posts_list:
+                filehandle.write('%s\n' % listitem["link"])
+
+        for post in unpublished_posts:
+            title = html.unescape(post["title"]["rendered"])
+            if len(title) > 230:
+                title = title[0: 230] + "..." + title[-21:]
+
+            link = post["link"]
+            time.sleep(1)
+            api.update_status(f"Hot deal:! {title} {link}")
+            time.sleep(60)
+
+
 api = create_api()
-
-while True:
-    previous_links = []
-    with open(os.path.join(os.getcwd(), "assets/previous_links.txt"), 'r') as filehandle:
-        for line in filehandle:
-            # remove linebreak which is the last character of the string
-            currentPlace = line[:-1]
-            previous_links.append(currentPlace)
-
-
-    response = requests.get("https://almostfree.me/wp-json/wp/v2/posts/")
-    posts_list = response.json()
-    unpublished_posts = list(filter(lambda x: x["link"] not in previous_links, posts_list))
-
-    with open("assets/previous_links.txt", 'w') as filehandle:
-        for listitem in posts_list:
-            filehandle.write('%s\n' % listitem["link"])
-
-    for post in unpublished_posts:
-        title = html.unescape(post["title"]["rendered"])
-        link = post["link"]
-        time.sleep(1)
-        api.update_status(f"Check out this new article{title} {link}")
-        time.sleep(60)
+if __name__ == '__main__':
+    main()
